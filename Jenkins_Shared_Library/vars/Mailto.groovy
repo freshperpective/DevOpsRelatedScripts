8cf62a27838a68d8f01ba3674a2dc7daa0b7588a
freshperpective <57676879+freshperpective@users.noreply.github.com>
2024-11-16 08:52:36 +0530

    Add files via upload

---
+++ Jenkins_Shared_Library/vars/Mailto.groovy
@@ -0,0 +1,25 @@
+def sdd(){
+emailext attachLog: true, body: '$DEFAULT_CONTENT', compressLog: true, postsendScript: '$DEFAULT_POSTSEND_SCRIPT', presendScript: '$DEFAULT_PRESEND_SCRIPT', recipientProviders: [culprits(), requestor(), developers(), previous()], replyTo: '$DEFAULT_REPLYTO', subject: '$DEFAULT_SUBJECT', to: '$DEFAULT_RECIPIENTS, abhishek.srivastava@saviorstj.com, santosh.kumar@saviorstj.com'
+
+}
+
+def edd1()
+{
+emailext attachLog: true, body: '$DEFAULT_CONTENT', compressLog: true, postsendScript: '$DEFAULT_POSTSEND_SCRIPT', presendScript: '$DEFAULT_PRESEND_SCRIPT', recipientProviders: [culprits(), requestor(), developers(), previous()], replyTo: '$DEFAULT_REPLYTO', subject: '$DEFAULT_SUBJECT', to: '$DEFAULT_RECIPIENTS, milan.sen@saviorstj.com, sanjay.pandey@saviorstj.com'
+
+}
+
+def erp(){
+emailext attachLog: true, body: '$DEFAULT_CONTENT', compressLog: true, postsendScript: '$DEFAULT_POSTSEND_SCRIPT', presendScript: '$DEFAULT_PRESEND_SCRIPT', recipientProviders: [culprits(), requestor(), developers(), previous()], replyTo: '$DEFAULT_REPLYTO', subject: '$DEFAULT_SUBJECT', to: '$DEFAULT_RECIPIENTS, vipin.singh@saviorstj.com, raju.kumar@saviorstj.com, praveen.dubey@saviorstj.com'
+
+}
+
+def qas(){
+emailext attachLog: true, body: '$DEFAULT_CONTENT', compressLog: true, postsendScript: '$DEFAULT_POSTSEND_SCRIPT', presendScript: '$DEFAULT_PRESEND_SCRIPT', recipientProviders: [culprits(), requestor(), developers(), previous()], replyTo: '$DEFAULT_REPLYTO', subject: '$DEFAULT_SUBJECT', to: '$DEFAULT_RECIPIENTS, hemant.kumar@saviorstj.com, sajneet.kaur@saviorstj.com'
+
+}
+
+def qah(){
+emailext attachLog: true, body: '$DEFAULT_CONTENT', compressLog: true, postsendScript: '$DEFAULT_POSTSEND_SCRIPT', presendScript: '$DEFAULT_PRESEND_SCRIPT', recipientProviders: [culprits(), requestor(), developers(), previous()], replyTo: '$DEFAULT_REPLYTO', subject: '$DEFAULT_SUBJECT', to: '$DEFAULT_RECIPIENTS, pratiq.kumar@saviorstj.com, ravinder.singh@saviorstj.com, rakesh.kumar@saviorstj.com'
+
+}
